2dadeb599b418317ffe2f6a1123c8ada3bfa5d61
Gerbor <gerjohnvandenbosch@hotmail.com>
2016-09-08 21:33:02 +0200

    Opening Cutscene
    Start of the opening Cutscene, Lights turning on via animation.

---
+++ Voxelated/Assets/Scripts/Scene Specific Scripts/OpeningCutSceneScripts/OpeningCutSceneOldMan.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpeningCutSceneOldMan : MonoBehaviour {
+    public Animator spotlights;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(StartWaitTimer(3, "WaitOnStart"));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator StartWaitTimer (int i, string s) {
+        yield return new WaitForSeconds(i);
+        switch (s) {
+            case "WaitOnStart":
+                LightsOn();
+                print("Turned Lights On");
+                break;
+            case "WaitForText":
+
+                break;
+        }
+    }
+
+    void LightsOn () {
+        spotlights.SetTrigger("On");
+        StartCoroutine(StartWaitTimer(2, "WaitForText"));
+    }
+}
