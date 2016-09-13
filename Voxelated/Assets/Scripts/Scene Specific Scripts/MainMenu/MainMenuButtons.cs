4e716c14233a58bbbb2f8dc1065e0a32cf42ff1b
Gerbor <gerjohnvandenbosch@hotmail.com>
2016-09-13 15:07:39 +0200

    Options Functioning
    Options menu is now openable

--- Voxelated/Assets/Scripts/Scene Specific Scripts/MainMenu/MainMenuButtons.cs
+++ Voxelated/Assets/Scripts/Scene Specific Scripts/MainMenu/MainMenuButtons.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class MainMenuButtons : MonoBehaviour {
-
+    public GameObject[] screens; //0 = mainscreen, 1 = options screen
+    /*
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +13,17 @@ public class MainMenuButtons : MonoBehaviour {
 	void Update () {
 	
 	}
+    */
+   public void Button (int i) {
+        switch (i) {
+            case 0:
+                screens[0].SetActive(false);
+                screens[1].SetActive(true);
+                break;
+            case 1:
+                screens[0].SetActive(true);
+                screens[1].SetActive(false);
+                break;
+        }
+    }
 }
