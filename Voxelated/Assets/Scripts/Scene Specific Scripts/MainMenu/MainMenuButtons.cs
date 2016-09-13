588f0d95ca744b55f1510ca2a8bd5175bb22d7e4
Gerbor <gerjohnvandenbosch@hotmail.com>
2016-09-13 16:10:32 +0200

    How i play game?
    Added black fade animation to main menu
    Made the new game button launch the game

--- Voxelated/Assets/Scripts/Scene Specific Scripts/MainMenu/MainMenuButtons.cs
+++ Voxelated/Assets/Scripts/Scene Specific Scripts/MainMenu/MainMenuButtons.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour {
     public GameObject[] screens; //0 = mainscreen, 1 = options screen
+    public Animator fadeAnim;
     /*
 	// Use this for initialization
 	void Start () {
@@ -24,6 +26,19 @@ public class MainMenuButtons : MonoBehaviour {
                 screens[0].SetActive(true);
                 screens[1].SetActive(false);
                 break;
+            case 2:
+                fadeAnim.SetTrigger("Black");
+                StartCoroutine(StartWaitTimer(1));
+                break;
         }
+    }
+
+    IEnumerator StartWaitTimer(int i) {
+        yield return new WaitForSeconds(i);
+        StartNewGame();
+    }
+
+    void StartNewGame () {
+        SceneManager.LoadScene(1);
     }
 }
