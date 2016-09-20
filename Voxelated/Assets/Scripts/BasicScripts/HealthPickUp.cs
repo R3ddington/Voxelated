a04cd431982efd952650e40d7537c142df20332c
Gerbor <gerjohnvandenbosch@hotmail.com>
2016-09-20 12:38:46 +0200

    Pickup scripts basic
    The basics of the pickup spawn systems

---
+++ Voxelated/Assets/Scripts/BasicScripts/HealthPickUp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPickUp : MonoBehaviour {
+
+    int health;
+    Transform location;
+    public GameObject prefab;
+
+    public HealthPickUp(int h, Transform pos) {
+        health = h;
+        location = pos;
+        //Do instantiate at pos
+    }
+}
---
+++ Voxelated/Assets/Scripts/BasicScripts/PickUpMaster.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PickUpMaster : MonoBehaviour {
+
+    int healthValue;
+   // Transform spawnLoc;
+    public List<Transform> healthSpawns = new List<Transform>();
+    public GameObject healthPrefab;
+
+	// Use this for initialization
+	void Start () {
+        Health();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Health () {
+        for(int i = 0; i < healthSpawns.Count; i++) {
+            // HealthPickUp health = new HealthPickUp(healthValue, healthSpawns[i]);
+            GameObject newHealth = Instantiate(healthPrefab, healthSpawns[i].position, Quaternion.identity) as GameObject;
+        }
+    }
+}
