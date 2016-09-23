4ec022c56d979f2e114298fada6a7705d6a32bd9
Gerbor <gerjohnvandenbosch@hotmail.com>
2016-09-23 09:42:47 +0200

    Begin of Coop movement
    Imported male Idle, walking and running animations
    Started on the Coop Player Controller
    Added Input for the following keys:
    Q
    W
    E
    A
    S
    D
    Arrow Up
    Arrow Down
    Arrow Left
    Arrow Right

---
+++ Voxelated/Assets/Scripts/Scene Specific Scripts/Coop/CoopPlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoopPlayerController : MonoBehaviour {
+
+    public GameObject player;
+    public Animator anim;
+    public int health;
+    public float speed;
+    public bool isPlayer1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ButtonInput();
+	}
+
+    void ButtonInput () {
+        if (isPlayer1)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+}
