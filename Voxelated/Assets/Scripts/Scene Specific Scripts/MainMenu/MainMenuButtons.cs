e40737ec16805e52113a009a4906e1c5ae8473ac
Gerbor <gerjohnvandenbosch@hotmail.com>
2016-10-06 19:38:15 +0200

    Options menu patch
    Changed the options menu to contain a tab for audio and grafics

--- Voxelated/Assets/Scripts/Scene Specific Scripts/MainMenu/MainMenuButtons.cs
+++ Voxelated/Assets/Scripts/Scene Specific Scripts/MainMenu/MainMenuButtons.cs
@@ -6,6 +6,7 @@ public class MainMenuButtons : MonoBehaviour {
     public GameObject[] screens; //0 = mainscreen, 1 = options screen, 2 = Coop menu, 3 = Credits screen
     public Animator fadeAnim;
     public GameObject[] creditText;
+    public GameObject[] optionPages; //0 = main page, 1 = audio, 2 = grafics
     /*
 	// Use this for initialization
 	void Start () {
@@ -61,6 +62,26 @@ public class MainMenuButtons : MonoBehaviour {
             creditText[o].SetActive(false);
         }
         creditText[i].SetActive(true);
+    }
+
+    public void OptionsMenu (int i)
+    {
+        switch (i)
+        {
+            case 0:
+                optionPages[0].SetActive(true);
+                optionPages[1].SetActive(false);
+                optionPages[2].SetActive(false);
+                break;
+            case 1:
+                optionPages[0].SetActive(false);
+                optionPages[1].SetActive(true);
+                break;
+            case 2:
+                optionPages[0].SetActive(false);
+                optionPages[2].SetActive(true);
+                break;
+        }
     }
 
     IEnumerator StartWaitTimer(int i, int id, int map) {
