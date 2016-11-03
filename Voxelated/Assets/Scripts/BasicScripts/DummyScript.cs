cbd4f9bd3043dfe27dd1e1f4ac359fd0b9c99d08
Gerbor <gerjohnvandenbosch@hotmail.com>
2016-11-03 15:29:03 +0100

    Hp bar update
    Added the HP bar to le dummies.
    Rescaled the katana hitboxes

--- Voxelated/Assets/Scripts/BasicScripts/DummyScript.cs
+++ Voxelated/Assets/Scripts/BasicScripts/DummyScript.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class DummyScript : MonoBehaviour {
     public Animator anim;
     public int health;
+    public int maxHealth;
+    public GameObject hpBar;
 
     public void Hit (int i)
     {
         health -= i;
+        hpBar.transform.GetComponent<LifeBar>().Damage(maxHealth, i);
         if (health <= 0)
         {
             //Play death animation
---
+++ Voxelated/Assets/Scripts/BasicScripts/LifeBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class LifeBar : MonoBehaviour
+{
+    public Image hpBar;
+
+    public void Damage(int life, int damage)
+    {
+        float hp;
+        hp = 1;
+        hp = hp/ life * damage;
+        hpBar.fillAmount -= hp;
+    }
+}
