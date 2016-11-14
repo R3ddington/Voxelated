2c916260e05c4d430563246d7d47be4faaac5133
DeesV <dees.veen@ehldeventer.nl>
2016-11-14 16:31:34 +0100

    Beetle Movement
    Script voor de Beetle van Lara (Triggered de animatie + geeft hem een
    forward).

---
+++ Voxelated/Assets/Scripts/BasicScripts/AmbientEnvAI/BeetleWalking.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeetleWalking : MonoBehaviour {
+
+    public Animator anim;
+    public float BeetleSpeed;
+
+	// Update is called once per frame
+	void Update () {
+        walkingBeetle();
+	}
+
+    void walkingBeetle() {
+        anim.SetBool("Walking", true);
+        transform.Translate(transform.forward * BeetleSpeed * Time.deltaTime);
+    }
+}
