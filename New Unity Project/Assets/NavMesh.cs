c2c0e2c0bd477e19cd30b1f05971f52b123bb48d
Gerbor <gerjohnvandenbosch@hotmail.com>
2016-11-15 13:42:42 +0100

    Chest update
    Added a chest + animations

---
+++ New Unity Project/Assets/NavMesh.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMesh : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public class Grid
+{
+    public int gridX;
+    public int gridY;
+    public Node[,] grid;
+
+    public void CreateGrid ()
+    {
+        grid = new Node[gridX, gridY];
+    }
+}
+
+public class Node
+{
+    bool walkable;
+}
---
+++ Voxelated/Assets/Scripts/InteractableScripts/OpenChest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenChest : MonoBehaviour {
+    public Animator anim;
+
+	public void Interact ()
+    {
+        anim.SetTrigger("Open");
+    }
+}
