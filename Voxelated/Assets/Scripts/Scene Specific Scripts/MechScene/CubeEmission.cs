e44a5dc54047c496647c66fdd454884e44834c49
Gerbor <gerjohnvandenbosch@hotmail.com>
2016-11-30 23:57:03 +0100

    Mech scene particles
    Added ambient particles to the mech scene
    Tested knockback on particles
    Tested animated emission

---
+++ Voxelated/Assets/Scripts/Scene Specific Scripts/MechScene/CubeEmission.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeEmission : MonoBehaviour {
+
+    public Renderer rend;
+    public Material mat;
+
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+        mat = rend.material;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float emission = Mathf.PingPong(Time.time, 5 - 2);
+        Color baseColor = Color.white;
+        Color finalColor = baseColor * Mathf.LinearToGammaSpace(emission);
+        mat.SetColor("_EmissionColor", finalColor);
+    }
+}
---
+++ Voxelated/Assets/Scripts/Scene Specific Scripts/MechScene/ParticleKnockBack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleKnockBack : MonoBehaviour {
+    public float force;
+    void OnParticleCollision(GameObject g)
+    {
+        print("triggered particle collision");
+        Rigidbody rb = g.GetComponent<Rigidbody>();
+        if(rb != null)
+        {
+            Vector3 dir = g.transform.position - transform.position;
+            dir = dir.normalized;
+            rb.AddForce(dir * force);
+        }
+    }
+}
