165457f36ea7fd9c777af5dc5895a88eb8f6170f
Gerbor <gerjohnvandenbosch@hotmail.com>
2016-12-06 13:39:47 +0100

    Level 2 patch
    Added more colliders to level 2
    Added more floating lava stones to level 2
    Fixed a bug where a character would have 4 guns

---
+++ Voxelated/Assets/Scripts/Scene Specific Scripts/Level2/SetLavaStream.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetLavaStream : MonoBehaviour {
+    public Animator anim;
+    public int type;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.SetInteger("Type",type);
+	}
+}
