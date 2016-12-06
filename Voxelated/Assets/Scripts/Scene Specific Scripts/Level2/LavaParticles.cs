1cc341e7b743609acc2118a8f55e2070fd409739
Gerbor <gerjohnvandenbosch@hotmail.com>
2016-12-06 01:50:31 +0100

    Level 2 Lava float stone
    Added a stone being pushed up by lava

---
+++ Voxelated/Assets/Scripts/Scene Specific Scripts/Level2/LavaParticles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaParticles : MonoBehaviour {
+    public int maxRate;
+    public float[] bursts;
+    // Use this for initialization
+    void Start()
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        var em = ps.emission;
+        em.enabled = true;
+
+        em.SetBursts(
+            new ParticleSystem.Burst[]{
+                new ParticleSystem.Burst(bursts[0], 100),
+                new ParticleSystem.Burst(bursts[1], 100),
+                new ParticleSystem.Burst(bursts[2], 100)
+            });
+    }
+}
---
+++ Voxelated/Assets/Scripts/Scene Specific Scripts/Level2/ParticlePushRock.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePushRock : MonoBehaviour {
+    public float force;
+    void OnParticleCollision(GameObject g)
+    {
+        if(g.transform.tag == "ParticleMove")
+        {
+            Rigidbody rb = g.GetComponent<Rigidbody>();
+            rb.AddForce(0, force, 0);
+        }
+    }
+}
