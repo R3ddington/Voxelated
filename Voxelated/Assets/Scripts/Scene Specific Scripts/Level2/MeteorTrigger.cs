2320efb0ef4040c7dc7c4c63964d30c2f2f91c0f
Gerbor <gerjohnvandenbosch@hotmail.com>
2016-12-22 21:09:33 +0100

    Level 2 fancy update
    Added a few more trees
    Changed the lava rock jumps, removed double rock jumps and changed them
    into a single big rock
    Moved some missplaced background rocks
    Imported the broken walls package
    Added broken walls to the meteor area

--- Voxelated/Assets/Scripts/Scene Specific Scripts/Level2/MeteorTrigger.cs
+++ Voxelated/Assets/Scripts/Scene Specific Scripts/Level2/MeteorTrigger.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MeteorTrigger : MonoBehaviour {
     public GameObject mSpawner;
     public int type;
+ //   public GameObject mech;
+  //  public Vector3 mechPos;
     void OnTriggerEnter(Collider c)
     {
         if(c.transform.tag == "Player")
@@ -13,11 +15,13 @@ public class MeteorTrigger : MonoBehaviour {
             {
                 case 0:
                     mSpawner.GetComponent<AimedMeteorite>().SetOn(c.gameObject);
+             //       mech.transform.position = 
                     break;
                 case 1:
                     mSpawner.GetComponent<AimedMeteorite>().SetOff();
                     break;
             }
+            Destroy(gameObject);
         }
     }
 }
