6f9b1c7c885b6090d5986b4682fec8d39d12a5ab
DeesV <dees.veen@ehldeventer.nl>
2016-12-22 13:53:25 +0100

    Skeleton  update

---
+++ TestingRound/Assets/Scripts/Hellbat/HellBat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HellBat : MonoBehaviour {
+
+    public float speedBat;
+
+	// Update is called once per frame
+	void Update () {
+        HBMovement();
+	}
+
+    public void HBMovement() {
+        transform.Translate(transform.forward * speedBat * Time.deltaTime);
+    }
+}
---
+++ TestingRound/Assets/Scripts/Skeleton/SkeletonAnimplusChange.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonAnimplusChange : MonoBehaviour {
+
+    public GameObject rayOrigin;
+    public RaycastHit playerHit;
+    public float rayLengthStanDet;
+    public float rayLengthMeleeDet;
+    public Animator skeletonAnim;
+    public float skeletonHP;
+    public GameObject skeletonHalf;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SkeletonMoAtt();
+        SkeletonHalfation();
+	}
+
+    public void SkeletonMoAtt() {
+        if (Physics.Raycast(rayOrigin.transform.position, rayOrigin.transform.forward, out playerHit, rayLengthStanDet)) {
+            skeletonAnim.SetBool("Running", true);
+            if (Physics.Raycast(rayOrigin.transform.position, rayOrigin.transform.forward, out playerHit, rayLengthMeleeDet)) {
+                skeletonAnim.SetTrigger("Attack");
+                skeletonAnim.SetBool("Running", false);
+                
+            }
+        }
+        else {
+            skeletonAnim.SetBool("Running", false);
+        }
+    }
+
+
+    public void SkeletonHalfation() {
+        if(skeletonHP <= 0) {
+            skeletonAnim.SetBool("Death", true);
+            StartCoroutine(destroyWait());
+             
+
+        }
+    }
+
+    IEnumerator destroyWait() {
+        yield return new WaitForSeconds(3);
+        Instantiate(skeletonHalf, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+}
