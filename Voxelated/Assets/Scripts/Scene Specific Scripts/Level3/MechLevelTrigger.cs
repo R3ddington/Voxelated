cbe7266021eb4fa40beed260b633fe3be3ad1f1a
Gerbor <gerjohnvandenbosch@hotmail.com>
2016-12-28 17:58:25 +0100

    Attached Mech level
    You can now reach every level and play through them

---
+++ Voxelated/Assets/Scripts/Scene Specific Scripts/Level3/MechLevelTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MechLevelTrigger : MonoBehaviour {
+    public Animator fade;
+    public bool activated;
+    void OnTriggerEnter(Collider c)
+    {
+        if(c.transform.tag == "Player")
+        {
+            if (activated)
+            {
+                return;
+            }
+            activated = true;
+            fade.SetTrigger("FadeOut");
+            StartCoroutine(WaitForLoad());
+        }
+    }
+    IEnumerator WaitForLoad()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(11);
+    }
+}
