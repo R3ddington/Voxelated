012ffcc1ca499a621c4b0dc54d53abe5e28b767a
Gerbor <gerjohnvandenbosch@hotmail.com>
2016-12-28 15:08:30 +0100

    Lava Boss battle basics
    Moved the boss area to make it line up with the level
    Lava boss can now walk, target the player, attack and die
    Added the lava boss health bar
    Added lava triggers below the boss platform
    Extended the anti fall to cover the boss area

---
+++ Voxelated/Assets/Scripts/Enemy Scripts/LavaBoss.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaBoss : MonoBehaviour {
+    public GameObject player;
+    public float speed;
+    public float attackRange;
+    public bool walking;
+    public Animator anim;
+    public float cooldown;
+    public bool cooling;
+    public GameObject armHitbox;
+    public GameObject hpBar;
+    public GameObject hpBarParent;
+
+    void Start ()
+    {
+        hpBarParent.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            Think();
+        }
+        else
+        {
+            GetPlayer();
+        }
+    }
+
+    public void GetPlayer()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void Think()
+    {
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist > attackRange)
+        {
+            Move();
+            if (!walking)
+            {
+                walking = true;
+                anim.SetBool("Walk", true);
+            }
+        }
+        else
+        {
+            walking = false;
+            anim.SetBool("Walk", false);
+            if (!cooling)
+            {
+                cooling = true;
+                StartCoroutine(Cooldown());
+                Attack();
+            }
+        }
+    }
+
+    public void Move()
+    {
+        Vector3 lookPos = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        transform.LookAt(lookPos);
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
+    }
+    public void Attack()
+    {
+        anim.SetTrigger("Punch");
+    }
+    public void AttackHitOn()
+    {
+        armHitbox.SetActive(true);
+    }
+    public void AttackHitOff()
+    {
+        armHitbox.SetActive(false);
+    }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        cooling = false;
+    }
+}
