19048f012ce0185ee946a0e8b422f3b84ee6e95e
Gerbor <gerjohnvandenbosch@hotmail.com>
2017-01-17 14:11:35 +0100

    Presentation preparations
    Added the presentation mode

---
+++ Voxelated/Assets/Scripts/DEVTOOLS/PresentationMode.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentationMode : MonoBehaviour {
+    public bool isPresentation;
+    public Vector3[] loc;
+    public GameObject player;
+    int step;
+	// Use this for initialization
+	void Start () {
+        if (isPresentation)
+        {
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this.transform);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ButtonInput();
+	}
+
+    public void ButtonInput()
+    {
+        if (Input.GetButtonDown("9"))
+        {
+            Skip();
+        }
+    }
+    public void Skip()
+    {
+        if(player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+        if(player != null)
+        {
+            player.transform.position = loc[step];
+            step++;
+        }
+    }
+}
